2e0748ece903b5add541aee65f7f93851535d153
Phil.adelphos <phil.adelphos.dev@gmail.com>
2022-05-30 16:42:08 +0200

    Add project files.

---
+++ Models/Pizzeria.cs
@@ -0,0 +1,15 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizzeria
+    {
+        public List<Pizza> listaPizze;
+        public Pizzeria()
+        {
+            listaPizze = new List<Pizza>();
+        }
+        public void addPizza(Pizza pizza)
+        {
+            this.listaPizze.Add(pizza);
+        }
+    }
+}
